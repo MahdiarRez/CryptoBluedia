2cf336eec1e33edd0b074ced614e8240058c091f
mahdiarrez <makhti733@gmail.com>
2025-01-09 12:58:26 +0330

    add - currencies page started

--- app/currencies/page.tsx
+++ app/currencies/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function Page() {
-  return <div>hello</div>;
+  return (
+    <div
+      className={` px-4 dark:bg-DarkBlue w-dvh max-h-dvh min-h-dvh sm:px-8 lg:px-28 xl:px-40 text-DarkBlue dark:text-WHITE bg-WHITE items-center justify-center flex flex-col`}
+    >
+      hi
+    </div>
+  );
 }
 
 export default Page;
