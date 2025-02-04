65aae0bf03470eb5fe2d6b9b1c07fce72ea75c52
mahdiarrez <makhti733@gmail.com>
2025-02-04 23:44:46 +0330

    fix - ts config fixed

--- app/currencies/[currency]/page.tsx
+++ app/currencies/[currency]/page.tsx
@@ -1,24 +1,31 @@
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
+//
+// export function generateMetadata({
+//   params,
+// }: {
+//   params: { currency: string };
+// }): Metadata {
+//   return {
+//     title: `${params.currency.toUpperCase()}`,
+//     description: `Displaying information for currency: ${params.currency}`,
+//   };
+// }
+//
+// export default function Page({ params }: { params: { currency: string } }) {
+//   console.log(params);
+//   return (
+//     <main className="h-screen w-full bg-white flex justify-center items-center p-4">
+//       <div className="text-center">
+//         <h1 className="text-2xl font-bold mb-4"></h1>
+//         {/* Add your currency-related content here */}
+//       </div>
+//     </main>
+//   );
+// }
+import React from 'react';
 
-export function generateMetadata({
-  params,
-}: {
-  params: { currency: string };
-}): Metadata {
-  return {
-    title: `${params.currency.toUpperCase()}`,
-    description: `Displaying information for currency: ${params.currency}`,
-  };
+function Page() {
+  return <div>hi</div>;
 }
 
-export default function Page({ params }: { params: { currency: string } }) {
-  console.log(params);
-  return (
-    <main className="h-screen w-full bg-white flex justify-center items-center p-4">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold mb-4"></h1>
-        {/* Add your currency-related content here */}
-      </div>
-    </main>
-  );
-}
+export default Page;
