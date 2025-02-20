02805167c41c0c51f25f4f77578c2f1da48c10a5
mahdiarrez <makhti733@gmail.com>
2025-02-20 13:20:27 +0330

    add - news page started

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
 import type { Metadata } from 'next';
+import { TabTransitionPanel } from '@/app/news/transitionPanel';
+import { TextGenerate } from '@/app/components/textGenerate';
 
 export const metadata: Metadata = {
-  title: 'Market News',
+  title: 'Latest News',
 };
 
 async function Page() {
-  return <div className={'bg-red-300 w-screen h-screen flex gap-5'}>page1</div>;
+  return (
+    <div
+      className={
+        'bg-WHITE w-dvh min-h-dvh flex text-DarkBlue flex-col justify-center items-center px-4 sm:px-8 lg:px-28 xl:px-40'
+      }
+    >
+      <div className={'flex flex-col items-center justify-center'}>
+        <TextGenerate
+          preset={'fade-in-blur'}
+          per={'line'}
+          classes={'text-3xl sm:text-4xl font-extrabold text-center'}
+          text={'Discover 219+ digital currencies.'}
+        />
+        <TextGenerate
+          preset={'fade'}
+          delay={0.8}
+          classes={
+            'mt-3 text-sm md:text-base lg:text-sm xl:text-base font-medium max-w-[324px] sm:max-w-2xl text-center'
+          }
+          text={
+            'Delve into a vast selection of digital currencies. Your journey into crypto starts here.'
+          }
+        />
+      </div>
+      {/*<FeaturesSection />*/}
+      <TabTransitionPanel />
+    </div>
+  );
 }
 
 export default Page;
