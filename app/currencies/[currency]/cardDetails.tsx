8c4a1abf415f3aec285a3ec39c96261fd8c10773
mahdiarrez <makhti733@gmail.com>
2025-04-09 11:53:01 +0330

    add - charts added to currency page

--- app/currencies/[currency]/cardDetails.tsx
+++ app/currencies/[currency]/cardDetails.tsx
@@ -1,19 +1,27 @@
 import { Currency } from '@/app/lib/utils/types';
-import { GrScorecard } from 'react-icons/gr';
+import { ReactElement } from 'react';
 
-function CardDetails({ currency }: { currency: Currency }) {
+function CardDetails({
+  currency,
+  title,
+  icon,
+  value,
+}: {
+  currency: Currency;
+  title: string;
+  icon: ReactElement;
+  value: string;
+}) {
   return (
     <div
       className="h-auto w-auto rounded-2xl flex flex-col p-5 items-start bg-white border border-solid"
       style={{ borderColor: currency.color }}
     >
-      <h5 className=" text-xl font-medium text-DarkBlue pb-2">
-        Currenct Value
-      </h5>
+      <h5 className=" text-xl font-medium text-DarkBlue/80 pb-2">{title}</h5>
       <span className="w-full h-px bg-DarkBlue/30"></span>
       <div className="flex flex-row items-center gap-3 mt-4">
-        <GrScorecard className="bg-purple-400 p-2 rounded-md text-4xl text-white" />
-        <span className=" text-3xl font-bold text-DarkBlue">$1298</span>
+        {icon}
+        <span className=" text-3xl font-bold text-DarkBlue">{value}</span>
       </div>
     </div>
   );
--- app/currencies/[currency]/cryptoBody.tsx
+++ app/currencies/[currency]/cryptoBody.tsx
@@ -1,15 +1,73 @@
 import { Currency } from '@/app/lib/utils/types';
 import CardDetails from './cardDetails';
+import { GrScorecard } from 'react-icons/gr';
+import { GoAlertFill } from 'react-icons/go';
+import { FaRankingStar } from 'react-icons/fa6';
+import { MdOutlineSentimentSatisfiedAlt } from 'react-icons/md';
+import { DonutChart, PriceHistoryChart } from './currencyCharts';
 
 function CryptoBody({ currency }: { currency: Currency }) {
+  const riskRewardData = [
+    { name: 'Risk', value: currency.risk },
+    { name: 'Reward', value: currency.reward },
+  ];
   return (
     <div className="flex flex-col items-center w-full">
-      <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 w-full gap-6 mt-8">
-        <CardDetails currency={currency} />
-        <CardDetails currency={currency} />
-        <CardDetails currency={currency} />
-        <CardDetails currency={currency} />
-        <CardDetails currency={currency} />
+      <div className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 w-full gap-6 mt-8">
+        <CardDetails
+          currency={currency}
+          icon={
+            <GrScorecard
+              className=" p-2 rounded-md text-4xl text-white"
+              style={{ backgroundColor: currency.color }}
+            />
+          }
+          title="Value"
+          value={`${currency.value}%`}
+        />
+        <CardDetails
+          currency={currency}
+          icon={
+            <GoAlertFill
+              className=" p-2 rounded-md text-4xl text-white"
+              style={{ backgroundColor: currency.color }}
+            />
+          }
+          title="Risk/Reward"
+          value={`${currency.risk}/${currency.reward}`}
+        />
+        <CardDetails
+          currency={currency}
+          icon={
+            <FaRankingStar
+              className=" p-2 rounded-md text-4xl text-white"
+              style={{ backgroundColor: currency.color }}
+            />
+          }
+          title="Rank 2025"
+          value={`#${currency.rank2025}`}
+        />
+        <CardDetails
+          currency={currency}
+          icon={
+            <MdOutlineSentimentSatisfiedAlt
+              className=" p-2 rounded-md text-4xl text-white"
+              style={{ backgroundColor: currency.color }}
+            />
+          }
+          title="Sentiment"
+          value={`${currency.sentiment}%`}
+        />
+      </div>
+      <div className="flex flex-col lg2:flex-row items-center w-full mt-7 gap-6">
+        <PriceHistoryChart currency={currency} />
+        <DonutChart
+          data={riskRewardData}
+          title="Risk vs Reward"
+          description="Balance between risk and potential reward"
+          colors={[currency.color, '#121212']} // rose and purple
+          valueFormatString="percent"
+        />
       </div>
     </div>
   );
